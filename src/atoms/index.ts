d4b229862004a70df2aeeef5906cbcbd0a08864a
doyoonear <doyoonleee@gmail.com>
2022-04-21 20:45:55 +0900

    refactor: Remove unused files

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,10 +1,4 @@
 import { atom, atomFamily } from 'recoil';
-import { Track } from '../types/common';
-
-export const trackListState = atom<Track[]>({
-  key: 'trackList',
-  default: [],
-});
 
 export const currentOpenIndexState = atom<number | null>({
   key: 'currentOpenIndex',
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import Layout from '@/components/Layout';
-import SideBar from '@/components/Sidebar';
-import AlbumList from '@/components/AlbumList';
 
 function Main() {
-  return (
-    <Layout>
-      <SideBar />
-      <AlbumList />
-    </Layout>
-  );
+  return <div></div>;
 }
 
 export default Main;
