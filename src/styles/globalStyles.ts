af486eaa5409dde62c84b5178052bd6ac3e518d2
Joeun <joyfuljoeunlee@gmail.com>
2022-04-23 18:01:33 +0900

    feature: Add TailwindCSS

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
-  
+
   * {
     box-sizing: border-box;
     word-wrap: break-word;
