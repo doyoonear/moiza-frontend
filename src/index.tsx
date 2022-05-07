073a154403c5fbf4177a6273db86c993699de660
Joeun <joyfuljoeunlee@gmail.com>
2022-05-07 15:01:45 +0900

    feature: route path에 starting-chat 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
+import { css, Global, ThemeProvider } from '@emotion/react';
 import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import { ThemeProvider, Global, css } from '@emotion/react';
+import NavBar from './components/Navigation/NavBar';
+import ChatRoom from './pages/ChatRoom';
+import ChatRoomDescription from './pages/ChatRoomDescription';
+import Main from './pages/Main';
 import baseTheme from './styles/baseTheme';
 import './styles/globals.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyles from './styles/globalStyles';
-
-import NavBar from '@/components/Navigation/NavBar';
-import Main from './pages/Main';
-import ChatRoom from './pages/ChatRoom';
 
 function RootWithCallbackAfterRender() {
   useEffect(() => {
@@ -30,6 +30,7 @@ function RootWithCallbackAfterRender() {
             <Routes>
               <Route path="/" element={<Main />} />
               <Route path="chatroom" element={<ChatRoom />} />
+              <Route path="starting-chatroom" element={<ChatRoomDescription />} />
             </Routes>
           </BrowserRouter>
         </RecoilRoot>
