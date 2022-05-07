87285d86dda4fa95c30fc5e32ed43f2cbdc0e366
jjung <iamhojjung@gmail.com>
2022-05-07 13:34:17 +0900

    -홈화면 bottom sheet 적용 -채팅방 레이아웃 적용

---
+++ src/components/BaseLayer.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+function BaseLayer() {
+  return (
+    <Contain>
+      <Title>지금 모일 수 있는 곳</Title>
+      <ChatList>
+        {[1, 2, 3, 4, 5, 6].map((el, idx) => {
+          return <ChatBox key={idx}></ChatBox>;
+        })}
+      </ChatList>
+    </Contain>
+  );
+}
+
+export default BaseLayer;
+
+const Contain = styled.div`
+  padding: 25px;
+`;
+
+const Title = styled.h2`
+  font-size: 28px;
+  line-height: 36px;
+  font-weight: 400;
+  margin-bottom: 25px;
+`;
+
+const ChatList = styled.div``;
+
+const ChatBox = styled.div`
+  padding: 15px;
+  background: #fff;
+  margin-bottom: 20px;
+`;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Home from '../components/Home';
 
 function Main() {
-  return <div></div>;
+  return <Home />;
 }
 
 export default Main;
