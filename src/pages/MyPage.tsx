8a434bd58e43220673eeb81f2532ef8282030bff
doyoonear <doyoonleee@gmail.com>
2022-05-14 15:27:33 +0900

    feature: Add Link to NavBar, Add mypage route, Add layout component

---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommonTitle from '@/components/Typography/Title';
+import Layout from '@/components/Layout';
+
+function MyPage() {
+  return (
+    <Layout>
+      <CommonTitle>마이페이지</CommonTitle>
+    </Layout>
+  );
+}
+
+export default MyPage;
