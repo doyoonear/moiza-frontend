76762721681f8dd21368d95d533c95fb6ffc2608
Joeun <joyfuljoeunlee@gmail.com>
2022-05-14 14:44:11 +0900

    feature: /auth 라우트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import NavBar from './components/Navigation/NavBar';
+import Auth from './pages/Auth';
 import ChatRoom from './pages/ChatRoom';
 import ChatRoomDescription from './pages/ChatRoomDescription';
 import Main from './pages/Main';
@@ -30,6 +31,7 @@ function RootWithCallbackAfterRender() {
               <Route path="/" element={<Main />} />
               <Route path="chatroom" element={<ChatRoom />} />
               <Route path="starting-chatroom" element={<ChatRoomDescription />} />
+              <Route path="auth" element={<Auth />} />
             </Routes>
           </BrowserRouter>
         </RecoilRoot>
