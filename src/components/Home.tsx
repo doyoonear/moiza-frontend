7c784e56890a8ac7dbeeaa5bedc58315121df164
jjung <iamhojjung@gmail.com>
2022-05-28 15:14:58 +0900

    - 접속한 위치의 위도 경도를 가져와 지도의 중심으로 잡아줌 - css단위 px->rem 으로 변경 - Home.tsx에서 불필요한 변수 삭제

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,17 +3,56 @@ import Sheet, { SheetRef } from 'react-modal-sheet';
 import styled from '@emotion/styled';
 import BaseLayer from './BaseLayer';
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+interface ILocation {
+  latitude: number;
+  longitude: number;
+}
+
 const Home = () => {
   const [isOpen, setIsOpen] = useState(true);
   const snapPoints = [700, 100];
   const initialSnap = snapPoints.length - 1;
-  const ref = useRef<SheetRef>();
-  const snapTo = (i: number) => ref.current?.snapTo(i);
   const [snapIndex, setSnapIndex] = useState<number>(1);
+  const { kakao } = window;
+  // 접속한 위치의 위도와 경도를 저장 할 변수
+  const [location, setLocation] = useState<ILocation>(null);
+
+  const getMyGps = () => {
+    return new Promise((resolve, rejected) => {
+      navigator.geolocation.getCurrentPosition(resolve, rejected);
+    });
+  };
+
+  const mapDrawer = async () => {
+    try {
+      let position: any = await getMyGps();
+      setLocation({ latitude: position.coords.latitude, longitude: position.coords.longitude });
+    } catch (err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+  };
 
   useEffect(() => {
-    console.log(snapTo);
-  }, [snapTo]);
+    getMyGps();
+    mapDrawer();
+  }, []);
+
+  useEffect(() => {
+    if (location !== null) {
+      const container = document.getElementById('myMap');
+      const options = {
+        center: new kakao.maps.LatLng(location.latitude, location.longitude),
+        level: 3,
+      };
+      const map = new kakao.maps.Map(container, options);
+    }
+  }, [location]);
 
   return (
     <Container>
@@ -30,7 +69,15 @@ const Home = () => {
           <Sheet.Header>
             <HeaderText>현재 카테고리 : 모두</HeaderText>
           </Sheet.Header>
-          <Sheet.Content>브ㄹㅏㅡ라</Sheet.Content>
+          <Sheet.Content>
+            <div
+              id="myMap"
+              style={{
+                width: '100%',
+                height: '45rem',
+              }}
+            ></div>
+          </Sheet.Content>
         </Sheet.Container>
       </Sheet>
     </Container>
@@ -40,15 +87,14 @@ const Home = () => {
 export default Home;
 
 const Container = styled.div`
-  width: 390px;
-  margin: 0 auto;
+  width: 100%;
   background: #ddd;
   height: 100vh;
 `;
 
 const HeaderText = styled.div`
-  padding: 12px 30px;
+  padding: 1.2rem 3rem;
   background-color: #ff8a00;
-  border-top-left-radius: 7px;
-  border-top-right-radius: 7px;
+  border-top-left-radius: 0.7rem;
+  border-top-right-radius: 0.7rem;
 `;
