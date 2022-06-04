ae393755408f26b98b1ab574888d28acc3da155a
doyoonear <doyoonleee@gmail.com>
2022-06-04 14:20:33 +0900

    feature: Add socket utils

---
+++ src/socket/server.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const botName = 'ChatBot';
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connets
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    socket.emit('message', formatMessage(botName, 'Welcome to chatcord!'));
+
+    // Broadcast when a user connects
+    socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${username} has joined the chat`));
+
+    // Emit Updated users after join
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chat message
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    // Send message to client side
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+
+      // Emit Updated users after leave
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}... `));
