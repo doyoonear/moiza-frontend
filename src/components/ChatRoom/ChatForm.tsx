78aabad5b90e8953811a1f9651a2b4d0b63cca74
doyoonear <doyoonleee@gmail.com>
2022-06-04 16:06:25 +0900

    redesign: Add form to chat room, Modify design

--- src/components/ChatRoom/ChatForm.tsx
+++ src/components/ChatRoom/ChatForm.tsx
@@ -1,14 +1,39 @@
 import React, { FormEventHandler } from 'react';
 import styled from '@emotion/styled';
+import SendIcon from '@/assets/send.svg';
 
-const ChatProfileImg = ({ inputHandler }: { inputHandler: FormEventHandler }): JSX.Element => {
-  return <StyledChatForm onInput={inputHandler} />;
+const ChatProfileImg = ({
+  inputHandler,
+  submitHandler,
+}: {
+  inputHandler: FormEventHandler;
+  submitHandler: FormEventHandler;
+}): JSX.Element => {
+  return (
+    <StyledChatForm onSubmit={submitHandler}>
+      <AbsoluteButton type="submit">
+        <SendIcon />
+      </AbsoluteButton>
+      <ChatInput onInput={inputHandler} />
+    </StyledChatForm>
+  );
 };
 
-const StyledChatForm = styled.input`
-  width: 6rem;
+const StyledChatForm = styled.form`
+  position: relative;
+`;
+
+const AbsoluteButton = styled.button`
+  position: absolute;
+  right: 1.5rem;
+  top: 1.5rem;
+`;
+
+const ChatInput = styled.input`
+  width: 100%;
   height: 6rem;
-  border-radius: 100%;
+  border: none;
+  border-radius: 3rem;
 `;
 
 export default ChatProfileImg;
--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import styled from '@emotion/styled';
 import CommonTitle from '@/components/Typography/Title';
 import ChatRoomRow from '@/components/ChatRoom/ChatRoomRow';
@@ -11,25 +11,37 @@ function ChatRoom() {
     setInputVal(currentTarget.value);
   };
 
+  const submitHandler = (event: FormEvent) => {
+    console.log('event', event);
+    event.preventDefault();
+  };
+
   return (
     <Layout>
       <StyledChatRoom>
-        <CommonTitle>채팅방 화면</CommonTitle>
-        <ChatRoomRow imgSrc="https://cdn.pixabay.com/photo/2022/01/29/06/07/couple-6976409_1280.jpg" isMyChat={false}>
-          {inputVal}
-        </ChatRoomRow>
-        <ChatRoomRow imgSrc="https://cdn.pixabay.com/photo/2022/01/29/06/07/couple-6976409_1280.jpg" isMyChat={true}>
-          아아앋가ㅓ다거
-        </ChatRoomRow>
-        <ChatForm inputHandler={inputHandler} />
+        <ChatWrapper>
+          <CommonTitle>채팅방 화면</CommonTitle>
+          <ChatRoomRow imgSrc="https://cdn.pixabay.com/photo/2022/01/29/06/07/couple-6976409_1280.jpg" isMyChat={false}>
+            {inputVal}
+          </ChatRoomRow>
+          <ChatRoomRow imgSrc="https://cdn.pixabay.com/photo/2022/01/29/06/07/couple-6976409_1280.jpg" isMyChat={true}>
+            아아앋가ㅓ다거
+          </ChatRoomRow>
+        </ChatWrapper>
+        <ChatForm inputHandler={inputHandler} submitHandler={submitHandler} />
       </StyledChatRoom>
     </Layout>
   );
 }
 
 const StyledChatRoom = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   background-color: ${({ theme }) => theme.color.primary};
-  height: 100vh;
+  height: 100%;
 `;
+
+const ChatWrapper = styled.div``;
 
 export default ChatRoom;
